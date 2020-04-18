a9ebca1c36949625373369a838f5a220839fd0b4
jnnieto <48180657+jnnieto@users.noreply.github.com>
2020-04-17 19:37:22 -0500

    Hola

---
+++ PROYECTO SAPPHIRO/App_Code/Encapsulate/EUsuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de EUsuario
+/// </summary>
+public class EUsuario
+{
+    public EUsuario()
+    {
+      
+    }
+}
